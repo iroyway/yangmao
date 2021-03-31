fefbed7f2a915ae437aab2636c41d1ec67e5a4c3
Chutsangkai <40229128+Chutsangkai@users.noreply.github.com>
2021-03-31 08:03:59 +0800

    更新crontab列表 修正脚本及日志名称 去除过期活动
    cron时间按照js脚本注释更改，脚本名称修正为与仓库统一，输出日志修正为脚本名称。

--- i-chenzhe/remote_crontab_list.sh
+++ i-chenzhe/remote_crontab_list.sh
@@ -1,23 +1,14 @@
 # 百变大咖秀
-0 10,11 * * 1-4 node /scripts/jd_entertainment.js >> /scripts/logs/jd_entertainment.log 2>&1
+10 10,11 * * 2-5 node /scripts/z_entertainment.js >> /scripts/logs/z_entertainment.log 2>&1
 
 # 粉丝互动
-15 10 * * * node /scripts/jd_fanslove.js >> /scripts/logs/jd_fanslove.log 2>&1
+3 10 * * * node /scripts/z_fanslove.js >> /scripts/logs/z_fanslove.log 2>&1
 
 # 超级摇一摇
-5 20 * * * node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1
-
-# 联想集卡活动
-10 15 15-29 3 * node /scripts/z_lenovo.js >> /scripts/logs/jd_lenovo.log 2>&1
+3 20 * * * node /scripts/z_shake.js >> /scripts/logs/z_shake.log 2>&1
 
 # 京东超市-大转盘
-10 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/jd_marketLottery.log 2>&1
+10 10 * * * node /scripts/z_marketLottery.js >> /scripts/logs/z_marketLottery.log 2>&1
 
 # 母婴-跳一跳
-10 8,14,20 17-25 3 * node /scripts/z_mother_jump.js >> /scripts/logs/jd_mother_jump.log 2>&1
-
-#答题赢京豆
-5 1 23-25 3 * node /scripts/z_grassy.js >> /scripts/logs/z_grassy.log 2>&1
-
-#京东小魔方
-10 10 25-27 3 * node /scripts/z_xmf.js >> /scripts/logs/z_xmf.log 2>&1
+5 8,14,20 25-31 3 * node /scripts/z_mother_jump.js >> /scripts/logs/z_mother_jump.log 2>&1
